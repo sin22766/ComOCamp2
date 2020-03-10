c0510e6ade6f4dfa673af1d5969adaa3e6cbef17
PCSHSCR <promchai2sin@gmail.com>
2020-03-10 14:09:53 +0700

    Hello

--- ComO_06.cpp
+++ ComO_06.cpp
@@ -8,18 +8,30 @@ struct Queue {
 
     void enq(int value) {
         while (!s1.empty()) {
+            cout << "move 1 2\n";
             s2.push(s1.top());
+            cout << s1.top() << "\n";
             s1.pop();
         }
+        cout << "push 1\n";
         s1.push(value);
         while (!s2.empty()) {
+            cout << "move 2 1\n";
             s1.push(s2.top());
+            cout << s2.top() << "\n";
             s2.pop();
         }
     }
 
-    void deq() {
-
+    int deq() {
+        if (s1.empty()) {
+            exit(0);
+        }
+        int x = s1.top();
+        cout << "pop 1\n";
+        cout << x << endl;
+        s1.pop();
+        return x;
     }
 };
 
